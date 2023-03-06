bb590174d37d77f336444a39846aff9a9bd0cf7a
dalerabduganiyev <126411318+dalerabduganiyev@users.noreply.github.com>
2023-03-06 22:33:04 +0500

    new projeck2

---
+++ Новая папка/main.js
@@ -0,0 +1,19 @@
+var css = document.querySelector('.btn');
+var aas = document.querySelector('.div3');
+var zzd = document.querySelector('.p2');
+var dzz = document.querySelector('.h2');
+var dzd = document.querySelector('.aa');
+var ddd = document.querySelector('.big');
+var zdz = document.querySelector('.p3');
+var zzz = document.querySelector('.p4');
+
+css.addEventListener('click' , function(){
+css.classList.toggle('new');
+aas.classList.toggle('new1')
+zzd.classList.toggle('p')
+dzz.classList.toggle('h')
+dzd.classList.toggle('aaa')
+zzz.classList.toggle('p')
+zdz.classList.toggle('p')
+ddd.classList.toggle('bigger')
+})
